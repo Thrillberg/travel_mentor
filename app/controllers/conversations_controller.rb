b747f39797d967aeb5d5f91c185adc590d074db2
Kevin Wang <knwang@gmail.com>
2015-08-04 12:32:12 -0400

    conversation model

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,37 +2,12 @@ class ConversationsController < ApplicationController
   before_action :require_user
 
   def index
-    all_users_messages
-    @conversations = @all_users_messages.group_by { |i| i.conversation_id }
+    @conversations = Conversation.find_all_involved_user(current_user)
   end
 
   def show
-    @users_messages = users_messages
-    @correspondent = correspondent[0]
+    @conversation = Conversation.find_by_id(params[:id])
   end
-
-  #def create
-  #  @new_conversation = Conversation.create(sender_id: current_user, recipient_id: @user)
-  #  require 'pry';binding.pry
-    #@new_conversation = Conversation.new
-    #@conversation.sender = current_user
-    #@conversation.recipient = @user
-
-  #  if @new_conversation.save
-  #    flash[:success] = "You have successfully sent your message."
-  #    redirect_to conversation_messages_path(@conversation)
-
-  #  end
-
-
-    #if Conversation.between(params[:sender_id], params[:recipient_id]).present?
-    #  @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
-    #else
-    #  require 'pry';binding.pry
-    #  @conversation = Conversation.new(conversation_params)
-    #end
-    #redirect_to conversation_messages_path(@conversation)
-  #end
 
   private
 
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,44 @@
 class Conversation
-  
+  attr_reader :messages
+
+  def initialize(messages)
+    @messages = messages
+  end
+
+  # def self.find_by_two_users(user1, user2)
+  #   @messages = Message.where("author_id = :user1_id AND reader_id = :user2_id", user1.id, user2.id).or.where("author_id = :user2_id AND reader_id = :user1_id", user1.id, user2.id)
+  #   new(@messages)
+  # end
+
+  def self.find_by_id(conversation_id)
+    @messages = Message.where("conversation_id = ?", conversation_id)
+    Conversation.new(@messages)
+  end
+
+  def self.find_all_involved_user(user)
+    messages = Message.where("author_id = :user_id OR reader_id = :user_id", {user_id: user.id)
+  end
+
+  def digest
+    # return the last message's body
+  end
+
+  def time_stamp_string(current_user)
+    # find the correspondence; interperlate correspondent time stamp of last message
+  end
+
+  def title(current_user)
+    the_other_user = the_other_user(current_user)
+    "Your Messages with #{the_other_user.first_name}"
+  end
+
+  def the_other_user(current_user)
+    # the user that is not the passed in user
+  end
+
+  def add_message(current_user)
+    the_other_user = the_other_user(current_user)
+    conversation_id = @messages.first.conversation_id
+    Message.create(author: current_user, reader: the_other_user, conversation_id: conversation_id, body)
+  end
 end
